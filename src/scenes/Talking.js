247335118940cc550a289243c5a78b010063e839
Astra Tsai <astra@ucsc.edu>
2025-02-28 18:30:15 -0800

    Add Nathan into the dialogue

--- src/scenes/Talking.js
+++ src/scenes/Talking.js
@@ -49,10 +49,11 @@ class Talking extends Phaser.Scene {
         this.nextText = this.add.bitmapText(this.NEXT_X, this.NEXT_Y, this.DBOX_FONT, '', this.TEXT_SIZE)
 
         // ready the character dialog images offscreen
-        this.homer = this.add.sprite(this.OFFSCREEN_X, this.DBOX_Y+8, 'homer').setOrigin(0, 1)
-        this.minerva = this.add.sprite(this.OFFSCREEN_X, this.DBOX_Y+8, 'minerva').setOrigin(0, 1)
-        this.neptune = this.add.sprite(this.OFFSCREEN_X, this.DBOX_Y+8, 'neptune').setOrigin(0, 1)
-        this.jove = this.add.sprite(this.OFFSCREEN_X, this.DBOX_Y+8, 'jove').setOrigin(0, 1)
+        this.homer = this.add.sprite(this.OFFSCREEN_X, this.DBOX_Y + 8, 'homer').setOrigin(0, 1)
+        this.minerva = this.add.sprite(this.OFFSCREEN_X, this.DBOX_Y + 8, 'minerva').setOrigin(0, 1)
+        this.neptune = this.add.sprite(this.OFFSCREEN_X, this.DBOX_Y + 8, 'neptune').setOrigin(0, 1)
+        this.jove = this.add.sprite(this.OFFSCREEN_X, this.DBOX_Y + 8, 'jove').setOrigin(0, 1)
+        this.nathan = this.add.sprite(this.OFFSCREEN_X, this.DBOX_Y + 8, 'nathan').setOrigin(0, 1)
 
         // input
         cursors = this.input.keyboard.createCursorKeys()
